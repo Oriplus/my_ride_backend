af7d2e87a6f88a54e7beea2c4d5e45c0934797a7
Oriana Morillo <orianacmv@gmail.com>
2023-10-21 21:30:14 -0300

    #3 feat(verify): add verify code

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,7 +7,7 @@ use App\Notifications\LoginNeedsVerification;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
-{    
+{
     /**
      * submit
      *
@@ -17,16 +17,40 @@ class LoginController extends Controller
     public function submit(Request $request): \Illuminate\Http\JsonResponse
     {
         $request->validate([
-            'phone' => 'required|numeric|min:10'
+            'phone' => 'required|min:10'
         ]);
         //will find a user or create
         $user = User::firstOrCreate([
             'phone' => $request->phone
         ]);
-        if(!$user){
-             return response()->json(['message' => 'Could not process a user with that phone number'], 401);
+        if (!$user) {
+            return response()->json(['message' => 'Could not process a user with that phone number'], 401);
         }
         $user->notify(new LoginNeedsVerification());
         return response()->json(['message' => 'Text message notification sent.']);
+    }
+    
+    /**
+     * verify code
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function verify(Request $request)
+    {
+        $request->validate([
+            'phone' => 'required|min:10',
+            'login_code' => 'required|numeric|between:111111,999999'
+        ]);
+        $user = User::where('phone', $request->phone)
+            ->where('login_code', $request->login_code)
+            ->first();
+        if ($user) {
+            $user->update([
+                'login_code' => null
+            ]);
+            return $user->createToken($request->login_code)->plainTextToken;
+        }
+        return response()->json(['message' => 'Invalid verification code'], 401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/login', [LoginController::class, 'submit']);
+Route::post('/login/verify', [LoginController::class, 'verify']);
+
+Route::group(['middleware' => 'auth:sanctum'], function (){
+  Route::get('/user', function(Request $request) {
+    return $request->user();
+  });
+});
